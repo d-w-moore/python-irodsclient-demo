6ec16070c881c5a13a49cfdd254f3c409fa53645
d-w-moore <dmoore@renci.org>
2023-09-27 00:33:24 -0400

    README

---
+++ README.md
@@ -0,0 +1,10 @@
+# python-irodsclient-demo
+
+These are some demonstrations of the [Python iRODS Client](http://github.com/irods/python-irodsclient) APIs.
+
+The demo\*.py scripts may be run directly - preferably beginning at demo1 and moving up in number...
+
+How to use:
+   1. Look at the output.
+   1. Trust your feelings
+   1. "Use the source, Luke."
--- irods_connection.py
+++ irods_connection.py
@@ -6,14 +6,14 @@ irods_version = tuple(int(_) for _ in irods.__version__.split('.'))
 get_home_collection = helpers.home_collection if irods_version > (1,0,0) else \
                       lambda session: '/{ses.zone}/home/{ses.username}'.format(ses = session)
 
-DEFAULTS = dict(host = 'localhost',
-                port = 1247,
-                user = 'rods',
-                password = 'rods',
-                zone = 'tempZone')
+_CONNECT_DEFAULTS = dict(host = 'localhost',
+                         port = 1247,
+                         user = 'rods',
+                         password = 'rods',
+                         zone = 'tempZone')
 
 def get(**auth_options):
-    options = DEFAULTS.copy()
+    options = _CONNECT_DEFAULTS.copy()
     options.update(auth_options)
     ses = irods.session.iRODSSession(**options)
     return [
